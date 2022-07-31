1ef15b5c2fd1900197354d4c14927d7353b5f257
Serand77 <sa.cibernautas@gmail.com>
2022-07-31 14:49:59 +0200

    Ajuste

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -13,31 +13,31 @@ public class MainMenu : MonoBehaviour
     public void Tenis()
     {
         boton.Play();
-        SceneManager.LoadScene(7);
+        SceneManager.LoadScene(4);
     }
     public void Beisbol()
     {
         boton.Play();
-        SceneManager.LoadScene(4);
+        SceneManager.LoadScene(2);
     }
     public void Bolos()
     {
         boton.Play();
-        SceneManager.LoadScene(5);
+        SceneManager.LoadScene(6);
     }
     public void Golf()
     {
         boton.Play();
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(5);
     }
     public void Boxeo()
     {
         boton.Play();
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(0);
     }
     public void Quite()
     {
         boton.Play();
-        SceneManager.LoadScene(6);
+        SceneManager.LoadScene(1);
     }
 }
